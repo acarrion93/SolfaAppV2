cdfedac634b4ac526383157213f9bd4b535a135c
acarrion93 <37372082+acarrion93@users.noreply.github.com>
2018-03-27 23:01:48 +0200

    clase usuario

---
+++ app/src/main/java/com/example/antonio/solfaapp/usuario.java
@@ -0,0 +1,17 @@
+package com.example.antonio.solfaapp;
+
+import java.net.PasswordAuthentication;
+
+/**
+ * Created by Antonio on 27/03/2018.
+ */
+
+public class usuario {
+    private String nombre;
+    private String apellidos;
+    private String dni;
+    private String password;
+    private int dineroPagar;
+    private boolean pagado;
+
+}
